06225be9d9edfc95443a1151728ce683fb531b16
mmaximm <scwh9447@gmail.com>
2023-11-14 02:37:48 +0900

    Initial Commit

---
+++ TodoAppTutorial/TodoAppTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTutorialApp.swift
+//  TodoAppTutorial
+//
+//  Created by ChoongWhan Shin on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
